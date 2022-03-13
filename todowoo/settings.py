2071a1fde28a15217d1e94817881047f5f367787
Himanshu Singh <himanshusinghskrpkvnp@Gmail.com>
2022-03-13 11:31:31 +0530

    minor commits

--- todowoo/settings.py
+++ todowoo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i)gh*5*#8a*zrmz1*(12cm#lsr$j)=m3wv$7ee@o)usi=(qbx6'
+SECRET_KEY = 'django-insecure-i)ghretretrtyereyyereyerryertretretyrvxgfffdhghfg*5*#8a*zrmz1*(12cm#lsr$j)=m3wv$7ee@o)usi=(qbx6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
